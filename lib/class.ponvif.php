d85febd3bd07d1859728e76c7949bcbb5ee67989
Flobul <flobul.jeedom@gmail.com>
2023-08-18 12:47:07 +0200

    Fix error on empty value and typo

--- lib/class.ponvif.php
+++ lib/class.ponvif.php
@@ -101,7 +101,7 @@ class Ponvif {
 		$this->deviceuri = $deviceuri;
 	}
 	public function getIPAddress() {
-		return $this->ipAddress;
+		return $this->ipaddress;
 	}
 	public function setIPAddress($ipAddress) {
 		$this->ipaddress = $ipAddress;
@@ -162,6 +162,7 @@ class Ponvif {
 
 	public function discover() {
 		$result = array();
+		$timeout = time() + $this->discoverytimeout;
 		$post_string = '<?xml version="1.0" encoding="UTF-8"?><e:Envelope xmlns:e="http://www.w3.org/2003/05/soap-envelope" xmlns:w="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dn="http://www.onvif.org/ver10/network/wsdl"><e:Header><w:MessageID>uuid:84ede3de-7dec-11d0-c360-f01234567890</w:MessageID><w:To e:mustUnderstand="true">urn:schemas-xmlsoap-org:ws:2005:04:discovery</w:To><w:Action a:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</w:Action></e:Header><e:Body><d:Probe><d:Types>dn:NetworkVideoTransmitter</d:Types></d:Probe></e:Body></e:Envelope>';
 		try {
 			if (false == ($sock = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP))) {
@@ -180,7 +181,6 @@ class Ponvif {
 			);
 			$sock_write = NULL;
 			$sock_except = NULL;
-
 			while (socket_select($sock_read, $sock_write, $sock_except, $this->discoverytimeout) > 0) {
 				if (false !== @socket_recvfrom($sock, $response, 9999, 0, $from, $this->discoverymcastport)) {
 					if ($response != NULL && $response != $post_string) {
@@ -226,6 +226,7 @@ class Ponvif {
 		$onvifVersion = $this->_getOnvifVersion($this->capabilities);
 		$this->mediauri = $onvifVersion['media'];
 		$this->deviceuri = $onvifVersion['device'];
+		$this->eventuri=$onvifVersion['event'];
 		$this->ptzuri = $onvifVersion['ptz'];
 		preg_match("/^http(.*)onvif\//", $this->mediauri, $matches);
 		$this->baseuri = $matches[0];
@@ -1052,7 +1053,9 @@ class Ponvif {
 		}
 		$version['media'] = $capabilities['Media']['XAddr'];
 		$version['device'] = $capabilities['Device']['XAddr'];
-		$version['event'] = $capabilities['Events']['XAddr'];
+		if (isset($capabilities['Events']['XAddr'])) {
+			$version['event'] = $capabilities['Events']['XAddr'];
+        }
 		if (isset($capabilities['PTZ']['XAddr'])) {
 			$version['ptz'] = $capabilities['PTZ']['XAddr'];
 		}
@@ -1270,4 +1273,3 @@ class Ponvif {
 	}
 
 }
-
